a95bbef0743c43aea9789d7985ea8f480fa6867e
GuidoGabrielePeruilh <58449852+GuidoGabrielePeruilh@users.noreply.github.com>
2025-12-04 19:51:08 -0300

    Handle bullet collisions after initial tick

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -13,6 +13,7 @@ namespace RedesGame.Bullets
         private NetworkRigidbody2D _myRigidBody;
         private TrailRenderer _trailRenderer;
         private GameObject _shooter;
+        private int _spawnTick;
 
 
         public override void Spawned()
@@ -20,6 +21,7 @@ namespace RedesGame.Bullets
             base.Spawned();
             _myRigidBody = GetComponent<NetworkRigidbody2D>();
             _trailRenderer = GetComponent<TrailRenderer>();
+            _spawnTick = Runner.Simulation.Tick;
         }
 
         private void DestroyBullet()
@@ -39,13 +41,31 @@ namespace RedesGame.Bullets
             _trailRenderer.enabled = active;
         }
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        private bool CanHandleCollision()
+        {
+            if (!Runner || !Runner.IsRunning) return false;
+            if (Runner.Simulation.Tick <= _spawnTick) return false;
+            return Object && Object.HasStateAuthority;
+        }
+
+        private void HandleCollision(Collider2D collision)
         {
             if (_shooter == collision.gameObject) return;
-            if (!Object && !Object.HasStateAuthority) return;
+            if (!CanHandleCollision()) return;
+
             collision.gameObject.GetComponent<IDamageable>()?.TakeForceDamage(_bulletData.ForceDamage, _myRigidBody.Rigidbody.velocity.normalized);
 
             DestroyBullet();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            HandleCollision(collision);
+        }
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            HandleCollision(collision);
         }
     }
 }
